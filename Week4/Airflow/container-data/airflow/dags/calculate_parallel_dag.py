9826f659a84725d751afe35ff03046146cc50cf1
AlexKbit <sav4enkoaleksandr@gmail.com>
2019-10-18 21:35:54 +0300

    Create Week4

---
+++ Week4/Airflow/container-data/airflow/dags/calculate_parallel_dag.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+
+def load():
+    return int(Variable.get('value'))
+
+
+def multiply(**ctx):
+    x = ctx['ti'].xcom_pull(key='return_value', task_ids='load_task')
+    x = x * 25
+    return x
+
+
+def plus5(**ctx):
+    x = ctx['ti'].xcom_pull(key='return_value', task_ids='multiply_task')
+    x = x + 5
+    return x
+
+
+def plus10(**ctx):
+    x = ctx['ti'].xcom_pull(key='return_value', task_ids='multiply_task')
+    x = x + 10
+    return x
+
+
+def upload1(**ctx):
+    x = ctx['ti'].xcom_pull(key='return_value', task_ids='plus_task')
+    Variable.set('result1', x)
+
+
+def upload2(**ctx):
+    x = ctx['ti'].xcom_pull(key='return_value', task_ids='plus10_task')
+    Variable.set('result2', x)
+
+
+dag = DAG(dag_id='calculation-parallel-dag',
+          start_date=datetime(2019, 10, 14),
+          schedule_interval='@once')
+
+load_task = PythonOperator(task_id='load_task',
+                           python_callable=load,
+                           dag=dag)
+multiply_task = PythonOperator(task_id='multiply_task',
+                               python_callable=multiply,
+                               provide_context=True,
+                               dag=dag)
+plus5_task = PythonOperator(task_id='plus5_task',
+                            python_callable=plus5,
+                            provide_context=True,
+                            dag=dag)
+plus10_task = PythonOperator(task_id='plus10_task',
+                             python_callable=plus10,
+                             provide_context=True,
+                             dag=dag)
+upload1_task = PythonOperator(task_id='upload1_task',
+                              python_callable=upload1,
+                              provide_context=True,
+                              dag=dag)
+upload2_task = PythonOperator(task_id='upload2_task',
+                              python_callable=upload2,
+                              provide_context=True,
+                              dag=dag)
+
+
+load_task >> multiply_task
+multiply_task >> plus5_task >> upload1_task
+multiply_task >> plus10_task >> upload2_task
---
+++ Week4/Airflow/container-data/airflow/dags/hello_dag.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+def print_1():
+    return 'Step 1'
+
+
+def print_2():
+    return 'Step 2'
+
+
+def print_3():
+    return 'Step 3'
+
+
+def print_hello():
+    return 'Hello Wolrd'
+
+
+dag = DAG('hello-world-dag',
+          start_date=datetime(2019, 10, 14),
+          catchup=False,
+          description='Hello world example',
+          schedule_interval='*/2 * * * *')
+
+step1 = PythonOperator(task_id='step1', python_callable=print_1, dag=dag)
+step2 = PythonOperator(task_id='step2', python_callable=print_2, dag=dag)
+step3 = PythonOperator(task_id='step3', python_callable=print_3, dag=dag)
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+step1 >> [step2, step3]
+step2 >> hello_operator
+step3 >> hello_operator
