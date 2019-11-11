5a0c9c6c4c3c1759827664c3e66de334f8457c24
AlexKbit <sav4enkoaleksandr@gmail.com>
2019-11-11 22:59:29 +0300

    Create hadoop image

--- Week2/Hadoop/init.bat
+++
@@ -1,6 +0,0 @@
-echo 'Copy require data to containers'
-docker cp data/checkdata.txt namenode:/tmp/checkdata.txt
-echo 'Upload to HDFS'
-docker exec -it namenode hdfs dfs -mkdir /data
-docker exec -it namenode hdfs dfs -put /tmp/checkdata.txt /data/checkdata.txt
-echo 'End'
--- Week2/Hadoop/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Copy require data to containers'
-docker cp data/checkdata.txt namenode:/tmp/checkdata.txt
-echo 'Upload to HDFS'
-docker exec -it namenode hdfs dfs -mkdir /data
-docker exec -it namenode hdfs dfs -put /tmp/checkdata.txt /data/checkdata.txt
-echo 'End'
